729bd0458b8858dcaad2ba05e9a261bed203888f
Nischal-YT <nischalnayak05@gmail.com>
2024-10-03 12:04:29 +0530

    framework

---
+++ src/main/java/com/thetesting/pojos/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.thetesting.pojos;
+
+public class TokenResponse {
+    // copy the token from postman - daa867e83584097
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- src/test/java/com/thetesting/base/BaseTest.java
+++ src/test/java/com/thetesting/base/BaseTest.java
@@ -1,9 +1,62 @@
 package com.thetesting.base;
 
+import com.thetesting.asserts.AssertActions;
+import com.thetesting.endpoints.APIConstants;
+import com.thetesting.modules.PayloadManager;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeTest;
 // Common to All to TestCase
 //  Base Test Father -> Testcase - Son - Single Inheritance
 
 public class BaseTest {
 
+    public RequestSpecification requestSpecification;
+    public AssertActions assertActions;
+    public PayloadManager payloadManager;
+    public JsonPath jsonPath;
+    public Response response;
+    public ValidatableResponse validatableResponse;
 
+    //  TC - Header
+
+    @BeforeTest
+    public void setUp() {
+        payloadManager = new PayloadManager();
+        assertActions = new AssertActions();
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(APIConstants.BASE_URL)
+                .addHeader("Content-Type", "application/json")
+                .build().log().all();
+
+//        requestSpecification = RestAssured.
+//                given()
+//                .baseUri(APIConstants.BASE_URL)
+//                .contentType(ContentType.JSON)
+//                .log().all();
+
+
+    }
+    public String getToken() {
+        requestSpecification = RestAssured
+                .given()
+                .baseUri(APIConstants.BASE_URL)
+                .basePath(APIConstants.AUTH_URL);
+
+        // Setting the payload
+        String payload = payloadManager.setAuthPayload();
+
+        // Get the Token
+        response = requestSpecification.contentType(ContentType.JSON).body(payload).when().post();
+
+        // String/Token Extraction
+        String token = payloadManager.getTokenFromJSON(response.asString());
+
+        return token;
+    }
 }
