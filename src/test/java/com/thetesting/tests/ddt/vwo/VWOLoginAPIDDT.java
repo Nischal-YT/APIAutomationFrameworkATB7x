27dc96a69ec32aa6f566baa1c12d5301989442cf
Nischal-YT <nischalnayak05@gmail.com>
2024-10-04 13:22:58 +0530

    DDT-apachepoi

---
+++ src/test/java/com/thetesting/tests/ddt/vwo/VWOLoginAPIDDT.java
@@ -0,0 +1,23 @@
+package com.thetesting.tests.ddt.vwo;
+
+import com.thetesting.utils.UtilsExcel;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class VWOLoginAPIDDT {
+
+    RequestSpecification r2;
+    ValidatableResponse vR2;
+    Integer ID;
+    Response res2;
+
+    @Test(dataProvider = "getData",dataProviderClass = UtilsExcel.class)
+    public void testVWOLogin(String email, String password) {
+        System.out.println(" -- Login API Testing");
+        System.out.println(email);
+        System.out.println(password);
+
+    }
+}
